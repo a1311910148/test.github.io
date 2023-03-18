660d7b7202897ad7eef1532e0284be57aacd9282
song <1311910148@qq.com>
2023-03-18 17:03:08 +0800

    1

--- server/copy.js
+++ server/copy.js
@@ -1,3 +1,3 @@
 const fs = require('fs');
-fs.writeFileSync('./dist/index.html', fs.readFileSync('./index.html', 'utf-8'))
+fs.writeFileSync('./dist/index.html', fs.readFileSync('./index.html', 'utf-8').replaceAll(' ', ''))
 console.log(fs.readFileSync('./index.html', 'utf-8'));
