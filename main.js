723720ecaecd1f3fb0285149ac40914118674b97
song <1311910148@qq.com>
2023-03-18 17:48:17 +0800

    1

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-
 let package1 = require('./src/modules/module1')
 let package2 = require('./src/modules/module2')
 let package3 = require('./src/modules/module3')
 
 console.log(package2, package3)
+console.log(package1.name, package1.age);
